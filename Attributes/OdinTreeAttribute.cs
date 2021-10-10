8d35a6fcf71c0a412f81ff8850c2483bdc031efa
André Felipe Siqueira <andrefs.995@gmail.com>
2021-10-10 17:04:08 -0300

    Initial commit

---
+++ Attributes/OdinTreeAttribute.cs
@@ -0,0 +1,6 @@
+﻿using ParadoxNotion.Design;
+
+/// <summary>
+/// Used to draw serialized classes using Odin's PropertyTree
+/// </summary>
+public class OdinTreeAttribute : DrawerAttribute { }
---
+++ Editor/OdinTreeAttributeDrawer.cs
@@ -0,0 +1,30 @@
+﻿using ParadoxNotion.Design;
+using Sirenix.OdinInspector.Editor;
+using System;
+using UnityEngine;
+
+/// <summary>
+/// Draw an object using the Property Tree of Odin
+/// </summary>
+public class OdinTreeAttributeDrawer : AttributeDrawer<OdinTreeAttribute> {
+        
+    private PropertyTree objectTree;
+    private object obj;
+            
+    public override object OnGUI(GUIContent content, object instance) {
+
+        // Create a instance if is null
+        if (instance == null) {
+            instance = Activator.CreateInstance(info.field.FieldType);
+        }
+
+        // Update the object
+        if (objectTree == null || obj != instance) {
+            obj = instance;
+            objectTree = PropertyTree.Create(instance);
+        }
+
+        objectTree.Draw(false);
+        return instance;
+    }
+}
